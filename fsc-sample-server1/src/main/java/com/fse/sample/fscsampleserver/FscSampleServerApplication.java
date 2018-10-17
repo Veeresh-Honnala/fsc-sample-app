62ea0cc219e4b8e42fb6874887eb65fa58666f39
Veeresh <kumar.hv3@gmail.com>
2018-10-17 21:57:31 +0530

    initial check in

---
+++ fsc-sample-server1/src/main/java/com/fse/sample/fscsampleserver/FscSampleServerApplication.java
@@ -0,0 +1,12 @@
+package com.fse.sample.fscsampleserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FscSampleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FscSampleServerApplication.class, args);
+	}
+}
---
+++ fsc-sample-server1/src/test/java/com/fse/sample/fscsampleserver/FscSampleServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fse.sample.fscsampleserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FscSampleServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
